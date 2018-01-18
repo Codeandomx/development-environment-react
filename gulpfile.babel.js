caf7bbb6be08062d7a96077de56566edba73b42f
Codeandomx <admin@codeando.org>
2018-01-17 20:31:30 -0600

    Creamos configuración para utilizar eslint para ayudar a mostrar errores de JSX en consola.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,12 +6,20 @@ import gutil from 'gulp-util';
 import browserify from 'browserify';
 import babel from 'gulp-babel';
 import connect from 'gulp-connect';
+import uglify from 'gulp-uglify';
+import eslint from 'gulp-eslint';
+
+const path = {
+    jsx: './app/src/**/*.jsx',
+    html: './app/*.html'
+};
 
 gulp.task('transform', () => {
-    return gulp.src('./app/src/**/*.jsx')
+    return gulp.src(path.jsx)
         .pipe(babel({
             presets: ["react", "env"]
         }))
+        .on('error', (e)=>{console.log('Error en JSX')})
         .pipe(gulp.dest('./app/dist'));
 });
 
@@ -20,7 +28,7 @@ gulp.task('js', ['transform'], () => {
         .bundle()
         .on('error', gutil.log)
         .pipe(source('main.js'))
-        .pipe(buffer())
+        .pipe(buffer(uglify()))
         .pipe(gulp.dest('./app'))
 });
 
@@ -29,16 +37,23 @@ gulp.task('connect', () => {
         root: 'app',
         port: 3000,
         livereload: true
-    });
+    })
 });
 
 gulp.task('html', () => {
-    gulp.src('./app/*.html')
+    gulp.src(path.html)
         .pipe(gulp.dest('./app'))
         .pipe(connect.reload());
 });
 
-gulp.task('default', ['js', 'connect'], () => {
-    gulp.watch('./app/**/*.jsx', ['js']);
-    gulp.watch('./app/*.html', ['html']);
+gulp.task('lint', function () {
+  return gulp.src(path.jsx)
+    .pipe(eslint('.eslintrc'))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['connect', 'js'], () => {
+    gulp.watch(path.jsx, ['js', 'lint']);
+    gulp.watch(path.html, ['html']);
 });
